6bc6523255df0cb5fd047e4069fba1a80ad6089b
ptrkfrsk <ptrqfrsq@gmail.com>
2020-11-20 11:44:56 +0100

    [Authentication] Created App, connected with Firebase and added user registration

---
+++ app/src/main/java/com/example/eventmaster/LoginActivity.kt
@@ -0,0 +1,34 @@
+package com.example.eventmaster
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
+
+        buttonSignUp.setOnClickListener{
+            startActivity(Intent(this, SignUpActivity::class.java))
+            finish()
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun updateUI(currentUser : FirebaseUser?) {
+
+    }
+}
---
+++ app/src/main/java/com/example/eventmaster/SignUpActivity.kt
@@ -0,0 +1,37 @@
+package com.example.eventmaster
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
+class SignUpActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+        auth = FirebaseAuth.getInstance()
+
+        buttonSuSignUp.setOnClickListener(){
+            createUser(editTextSuEmail.text.toString(), editTextSuPassword.text.toString())
+        }
+    }
+
+    private fun createUser(email : String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(baseContext, "Pomyślnie zarejestrowano", Toast.LENGTH_SHORT).show()
+                    val user = auth.currentUser
+                    startActivity(Intent(this, LoginActivity::class.java))
+                } else {
+                    Toast.makeText(baseContext, "Nie udało się zarejestrować", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+}
